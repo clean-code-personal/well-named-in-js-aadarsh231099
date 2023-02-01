922b4d509d39735f54912bf2ba03a092ddcd5ae5
Aadarsh <aadarshhanu23@gmail.com>
2023-02-01 15:13:15 +0530

    split into 3 files

---
+++ app/color_pair.js
@@ -0,0 +1,8 @@
+function ColorPair(){
+    this.majorColor;
+    this.minorColor;
+}
+
+ColorPair.prototype.toString=function(){
+    return `MajorColor:${this.majorColor}, MinorColr:${this.minorColor}`;
+}
