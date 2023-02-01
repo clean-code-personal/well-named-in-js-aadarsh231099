2afc6e7df834586d1c1a8bda571a010ef6e36aff
Aadarsh <aadarshhanu23@gmail.com>
2023-02-01 15:32:57 +0530

    c2

--- app/color_code.js
+++ app/color_code.js
@@ -9,7 +9,7 @@ function getColorFromPairNumber(pairNumber)
 {
     let minorSize = MajorColorNames.length;
     let majorSize = MinorColorNames.length;
-    
+
     if (pairNumber < 1 || pairNumber > minorSize * majorSize)
     {
         throw `Argument PairNumber:${pairNumber} is outside the allowed range` 
@@ -44,7 +44,7 @@ function getPairNumberFromColor(pair)
                 break;
             }
         }
-    
+
     if (majorIndex == -1 || minorIndex == -1)
         {
             throw `Unknown Colors:${pair.toString()}`;
